3d5435cb9787b4fd68d04035a21f0fc38b6a7565
Mathieu <mathieu.sirere@gmail.com>
2020-05-10 20:53:46 +0200

    Add min/max x/y

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -6,10 +6,25 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
 
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBounders();
+    }
+
+    private void SetUpMoveBounders()
+    {
+        var gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
     }
 
     // Update is called once per frame
@@ -23,8 +38,8 @@ public class Player : MonoBehaviour
         //To fix the fra
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         this.transform.position = new Vector2(newXPos, newYPos);
     }
 }
