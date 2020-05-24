d568de956b0c7e43d2744e2c41e3351ba909a1cb
Mathieu <mathieu.sirere@gmail.com>
2020-05-24 15:19:58 +0200

    Looping wave

--- LaserDefender/Assets/Scripts/EnemySpawner.cs
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -6,10 +6,14 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<WaveConfig> waveConfigs;
     [SerializeField] int startingWave = 0;
+    [SerializeField] private bool looping = false;
 
-    void Start()
+    IEnumerator Start()
     {
-        StartCoroutine(SpawnAllWaves());
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
     }
 
     private IEnumerator SpawnAllWaves()
