aa1d6df5e7025eff16a4b84f7bf1e587d8881ded
Mathieu <mathieu.sirere@gmail.com>
2020-06-06 17:46:40 +0200

    Add music

---
+++ LaserDefender/Assets/Scripts/MenuMusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        //If find objet of MenuMusicPlayer type
+        if (FindObjectsOfType(this.GetType()).Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
